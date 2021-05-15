34480df2b7dd165957ead0df14cb3214a8c8a0f1
audrec <audreycjob@gmail.com>
2021-05-14 19:33:57 -0700

    Update abstract methods for GameLevel class, add timer, score counting, and hint features.

--- src/com/masterMindGame/exceptionHandlers/InvalidResponseException.java
+++ src/com/masterMindGame/exceptionHandlers/InvalidResponseException.java
@@ -1,9 +1,6 @@
 package com.masterMindGame.exceptionHandlers;
 
 public class InvalidResponseException extends Exception{
-    public InvalidResponseException(String message, Throwable cause) {
-        super(message, cause);
-    }
     public InvalidResponseException(String message) {
         super(message);
     }
